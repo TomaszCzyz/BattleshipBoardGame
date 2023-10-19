7a72f4b927d079889f0e7cdb7c2c81bae537b1ca
tczyz <tomczyz97@gmail.com>
2023-10-19 16:29:41 +0200

    Change sdk to web and setup WebApplication

--- src/BattleshipBoardGame/Program.cs
+++ src/BattleshipBoardGame/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#pragma warning disable CA1848
+using System.Globalization;
+using Serilog;
 
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, configuration) =>
+    configuration
+        .MinimumLevel.Information()
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.Console(
+            outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext}]{NewLine} {Message:lj}{NewLine}{Exception}",
+            formatProvider: CultureInfo.CurrentCulture));
+
+var app = builder.Build();
+app.Logger.LogInformation("Application created. Launching application...");
+
+await app.RunAsync();
+
+Log.CloseAndFlush();
