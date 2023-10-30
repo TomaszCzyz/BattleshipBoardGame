3f5d0f9e8b9b78fb0fab54653a9b64a6903e50f8
tczyz <tomczyz97@gmail.com>
2023-10-30 09:49:22 +0100

    Fetch list of existing simulations from backend

--- frontend/src/components/SimulationsMenu.tsx
+++ frontend/src/components/SimulationsMenu.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export default SimulationsMenu;
 
 
 function SimulationsMenu() {
-  const simulations: string[] = ["sim#1", "sim#2", "sim_with_custom_name"];
+  const [simulationNames, setSimulationNames] = useState<string[]>(["sim_placeholder"]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/simulations/battleship/')
+      .then((response) => response.json())
+      .then((data) => setSimulationNames(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
-    <div className="SimulationsMenu" >
+    <div className="SimulationsMenu">
       <fieldset>
         <legend>Menu</legend>
         <fieldset className="SimulationsMenu-inner-fieldset">
           <legend>Select existing</legend>
-          <select placeholder="select simulation">
-            {simulations.map((sim) => <option key={sim} value={sim}>{sim}</option>)}
+          <select placeholder="select simulation" defaultValue="placeholder">
+            <option value="placeholder" disabled>Select sim</option>
+            {simulationNames.map((sim) => <option key={sim} value={sim}>{sim}</option>)}
           </select>
           <button className="SimulationsMenu-button">Load</button>
         </fieldset>
@@ -34,13 +42,13 @@ function PlayerSettings({name}: { name: string }) {
   return (
     <fieldset style={{border: "1px solid"}}>
       <legend>Player {name}</legend>
-      <text className="medium-text">Ships placement strategy:</text>
+      <span className="medium-text">Ships placement strategy:</span>
       <br/>
       <select>
         <option value="random">random</option>
       </select>
       <br/>
-      <text className="medium-text">Guessing strategy:</text>
+      <span className="medium-text">Guessing strategy:</span>
       <br/>
       <select>
         <option value="random">random</option>
